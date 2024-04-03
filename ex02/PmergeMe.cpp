b0722bfeb93718ac43ae4a38ef77983058d79898
Willem van der Vorst <willemvdvorst@gmail.com>
2024-04-03 18:01:48 +0200

    HI

--- ex02/PmergeMe.cpp
+++ ex02/PmergeMe.cpp
@@ -1,32 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.cpp                                           :+:      :+:    :+:   */
+/*   PmergeMe.cpp                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: wvan-der <wvan-der@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/04/03 18:00:58 by wvan-der          #+#    #+#             */
-/*   Updated: 2024/04/03 18:00:59 by wvan-der         ###   ########.fr       */
+/*   Created: 2024/04/03 18:01:30 by wvan-der          #+#    #+#             */
+/*   Updated: 2024/04/03 18:01:31 by wvan-der         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "BitcoinExchange.hpp"
-
-#include <exception>
-#include <iostream>
-#include <stdexcept>
-
-int main(int argc, char** argv)
-{
-	try {
-		if (argc != 2)
-		{
-			throw std::runtime_error("Need one file as argument!");
-		}
-		BitcoinExchange::doExchange(argv[1]);
-	} catch (std::exception& e) {
-		std::cerr << "Error: "<< e.what() << std::endl;
-		return 1;
-	}
-	return 0;
-}
